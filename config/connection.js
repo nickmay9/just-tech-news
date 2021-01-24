3aef597ead171d9b14f24eaaa3442a92cb6a8f83
Nick May <nickmay9@outlook.com>
2021-01-24 18:52:08 -0500

    create .env file

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'aaaaaa', {
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
