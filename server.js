9081fac49d71cc77caf34cc6a6889b6676a77ea3
Nick May <nickmay9@outlook.com>
2021-02-02 09:48:52 -0500

    finished feature partial and helpers

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const helpers = require('./utils/helpers');
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
